32d585ba8165df4caa9b89701e996ca3d7563a33
Shehryar Ehtesham <se318@rutgers.edu>
2020-03-27 00:31:58 -0400

    added fetch Issue function

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+function fetchIssues() {
+
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+
+    issuesList.innerHTML = '';
+
+    for (var i = 0; i < issues.length; i++) {
+        var id = issues[i].id;
+        var desc = issues[i].description;
+        var severtity = issues[i].severtity;
+        var asssignedTo = issues[i].asssignedTo;
+        var status = issues[i].status;
+
+        issuesList.innerHTML += '<div class="well">'+
+                                '<h6>Issues ID: ' + id + '</h6>'+
+                                '<p><span class="label-info">' + status + '<span></p>' + '<h3>' + desc + '</h3>' + '<p><span class="glyphicon glyphicon-time"></span>' + severtity + '</p>' + '<p><span class="glyphicon glyphicon-user"></span>'
+    }
+}
