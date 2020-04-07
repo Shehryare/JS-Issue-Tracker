4e8f9cde6b156ef72d7af338fbe6950f19149547
Shehryar Ehtesham <se318@rutgers.edu>
2020-04-07 08:16:52 -0400

    completed issues functionality

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-document.getElementById('issueInputForm').addEventListener('submit', saveIsue);
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
 
 function saveIssue(e) {
     var issueDesc = document.getElementById('issueDescInput').value;
@@ -30,33 +30,55 @@ function saveIssue(e) {
     e.preventDefault();
 }
 
-function setStatusClosed(id){
-var issues = JSON.parse()
+function setStatusClosed(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues[i].status = 'Closed'
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
+
+
+function deleteIssue(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++) {
+        if (issues[i].id == id) {
+            issues.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
 }
 
 
 function fetchIssues() {
-
     var issues = JSON.parse(localStorage.getItem('issues'));
     var issuesList = document.getElementById('issuesList');
-
+  
     issuesList.innerHTML = '';
-
+  
     for (var i = 0; i < issues.length; i++) {
-        var id = issues[i].id;
-        var desc = issues[i].description;
-        var severity = issues[i].severtity;
-        var assignedTo = issues[i].asssignedTo;
-        var status = issues[i].status;
-
-        issuesList.innerHTML += '<div class="well">' +
-            '<h6>Issue ID: ' + id + '</h6>' +
-            '<p><span class="label label-info">' + status + '</span></p>' +
-            '<h3>' + desc + '</h3>' +
-            '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>' +
-            '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
-            '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning">Close</a> ' +
-            '<a href="#" onclick="deleteIssue(\'' + id + '\')" class="btn btn-danger">Delete</a>' +
-            '</div>';
+      var id = issues[i].id;
+      var desc = issues[i].description;
+      var severity = issues[i].severity;
+      var assignedTo = issues[i].assignedTo;
+      var status = issues[i].status;
+  
+      issuesList.innerHTML +=   '<div class="well">'+
+                                '<h6>Issue ID: ' + id + '</h6>'+
+                                '<p><span class="label label-info">' + status + '</span></p>'+
+                                '<h3>' + desc + '</h3>'+
+                                '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
+                                '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                                '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                                '</div>';
     }
 };
